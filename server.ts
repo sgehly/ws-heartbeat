30d389ca9f40a4b7954226ed49fffc12af83dbee
Sam Gehly <sam@gehly.net>
2018-01-16 15:56:15 -0500

    Added optional callback for further cleanup.

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { WebSocket } from "./types";
 
-export function setWsHeartbeat(wss: WebSocket.Server, pong: (ws: WebSocket, data: any, binary: boolean) => void, pingTimeout: number = 60000) {
+export function setWsHeartbeat(wss: WebSocket.Server, pong: (ws: WebSocket, data: any, binary: boolean) => void, pingTimeout: number = 60000, callback: (ws: WebSocket) => void) {
     const connections = new Set<WebSocket>();
     wss.addListener("connection", ws => {
         connections.add(ws);
@@ -13,6 +13,7 @@ export function setWsHeartbeat(wss: WebSocket.Server, pong: (ws: WebSocket, data
         for (const ws of wss.clients) {
             if (ws.readyState === ws.CONNECTING || ws.readyState === ws.OPEN) {
                 if (!connections.has(ws)) {
+                    callback(ws);
                     ws.close();
                 }
             }
